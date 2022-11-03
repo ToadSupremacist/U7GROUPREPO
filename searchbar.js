2ad5f8afa6c29d6e89a398162c2255d34b5aaabf
ToadSupremacist <104451465+ToadSupremacist@users.noreply.github.com>
2022-11-03 17:30:50 +0500

    переключение по клавишам вверх/вниз

--- searchbar.js
+++ searchbar.js
@@ -1,23 +1,46 @@
-const srchmenu = document.querySelector(".searchmenu")
-const srchinput = document.querySelector(".search__input")
-const srchmenu_items = srchmenu.querySelectorAll(".searchmenu_item")
-const main_area = document.querySelector(".main")
+const srchmenu = document.querySelector(".searchmenu");
+const srchinput = document.querySelector(".search__input");
+const srchmenu_items = srchmenu.querySelectorAll(".searchmenu_item");
+const main_area = document.querySelector(".main");
+const form = document.querySelector(".search");
 
-const itemsArray = Array.from(srchmenu_items)
+
 
 function showSearch() {
+    let i = 1
     srchmenu.classList.remove("searchmenu--closed")
-    for (let i = 0; itemsArray.length; i++) {
-        itemsArray[i].setAttribute("tabindex", i)
+    for (el of srchmenu_items) {
+        el.setAttribute("tabindex", i)
+        i++
     }
-}
+};
 
 function hideSearch() {
     srchmenu.classList.add("searchmenu--closed")
-    for (let i = 0; itemsArray.length; i++) {
-        itemsArray[i].setAttribute("tabindex", -1)
+    for (el of srchmenu_items) {
+        el.setAttribute("tabindex", -1)
     }
-}
+};
 
-main_area.addEventListener("click", hideSearch)
-srchinput.addEventListener("input", showSearch)
+let i = 0;
+
+form.addEventListener("keydown", (event) => {
+    if (event.code == "ArrowDown") {
+        event.preventDefault()
+        srchmenu_items[i].focus({preventScroll:true})
+        i += 1
+    }
+});
+
+form.addEventListener("keydown", (event) => {
+    if (event.code == "ArrowUp") {
+        event.preventDefault()
+        srchmenu_items[i-1].focus({preventScroll:true})
+        i -= 1
+    }
+});
+
+main_area.addEventListener("click", hideSearch);
+srchinput.addEventListener("input", showSearch);
+
+
